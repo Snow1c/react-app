ea0844c4d518f6c5a7139123bd257c40b146f273
Sergey Dzyuba <ser.dzuba@mail.ru>
2021-10-06 18:30:03 +0300

    Delete PostItem animation added

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {createRef, useRef} from 'react'
 import MyButton from './UI/button/MyButton'
 
 export default function PostItem(props) {
-
+  const ref = useRef()
   return (
-    <div className="post">
+    <div ref={ref} className="post">
       <div className="post__content">
         <strong>{props.number}. {props.post.title}</strong>
         <div>
@@ -12,7 +12,13 @@ export default function PostItem(props) {
         </div>
       </div>
       <div className="post__btns">
-        <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
+        <MyButton onClick={() => {
+          ref.current.classList.toggle('delete')
+          // Для анимации
+          setTimeout(() => {
+            props.remove(props.post)
+          }, 300)
+        }}>Удалить</MyButton>
       </div>
     </div>
   )
